64133c9aa34f7fb77e56c83ef019a77cfcacc0c4
Rob Prouse <rob@prouse.org>
2018-03-08 07:30:37 -0800

    Add a test example of generic test fixtures

---
+++ DataDrivenTests/GenericTestClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace DataDrivenTests
+{
+    [TestFixture(typeof(int))]
+    [TestFixture(typeof(string))]
+    public class GenericTestClass<T>
+    {
+        [Test]
+        public void TestType()
+        {
+            Assert.Pass($"The generic test type is {typeof(T)}");
+        }
+    }
+}
