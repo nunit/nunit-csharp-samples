f9fd2035a332e6bbce6ccdd4eb2b8ba5f0d9fe8f
Lukasz Zasada <lza@scalepoint.com>
2019-10-28 10:13:01 +0100

    added comments to TimeoutRetryAttributeTests

--- TimeoutRetryAttributeExample/TimeoutRetryAttributeTests.cs
+++ TimeoutRetryAttributeExample/TimeoutRetryAttributeTests.cs
@@ -8,6 +8,7 @@ namespace TimeoutRetryAttributeExample
     [TestFixture]
     public class TimeoutRetryAttributeTests
     {
+        // Retry counters for corresponding test methods
         readonly Dictionary<string, int> retryCount = new Dictionary<string, int>()
         {
             { "RetryIsFiredDueToTimeout", 0 },
@@ -16,42 +17,46 @@ namespace TimeoutRetryAttributeExample
             { "RetryIsNotFiredDueToException", 0 }
         };
 
+        // This test should execute 3 times and fail due to reached retry number limit
         [Test]
         [MaxTime(1000)]
         [TimeoutRetry(3)]
         public void RetryIsFiredDueToTimeout()
         {
             TestContext.Out.WriteLine($"Running test for the {++retryCount["RetryIsFiredDueToTimeout"]} time");
-            Thread.Sleep(3000);
+            Thread.Sleep(3000); // Wait more than MaxTime threshold
         }
 
+        // This test should not retry at all and pass within first execution
         [Test]
         [MaxTime(3000)]
         [TimeoutRetry(5)]
         public void RetryIsNotFiredDueToLackOfTimeout()
         {
             TestContext.Out.WriteLine($"Running test for the {++retryCount["RetryIsNotFiredDueToTimeout"]} time");
-            Thread.Sleep(1000);
+            Thread.Sleep(1000); // Wait less than MaxTime threshold
         }
 
+        // This test should not retry due to failed assertion despite having reached the MaxTime threshold
         [Test]
         [MaxTime(1000)]
         [TimeoutRetry(10)]
         public void RetryIsNotFiredDueToFailedAssertion()
         {
             TestContext.Out.WriteLine($"Running test for the {++retryCount["RetryIsNotFiredDueToFailedAssertion"]} time");
-            Thread.Sleep(3000);
-            Assert.Fail("I failed");
+            Thread.Sleep(3000); // Wait more than MaxTime threshold
+            Assert.Fail("I failed"); // Introduce assertion failure
         }
 
+        // This test should not retry due to thrown exception despite having reached the MaxTime threshold
         [Test]
         [MaxTime(1000)]
         [TimeoutRetry(2)]
         public void RetryIsNotFiredDueToException()
         {
             TestContext.Out.WriteLine($"Running test for the {++retryCount["RetryIsNotFiredDueToException"]} time");
-            Thread.Sleep(3000);
-            throw new Exception("Exception thrown");
+            Thread.Sleep(3000); // Wait more than MaxTime threshold
+            throw new Exception("Exception thrown"); // Throw exception
         }
     }
 }
